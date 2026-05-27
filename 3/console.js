4885478ca391c35eef9cd0de37780fa2e2855a63
bobbychansfu <119755911+bobbychansfu@users.noreply.github.com>
2026-05-27 23:24:48 +0000

    initial node demo

---
+++ 3/console.js
@@ -0,0 +1,6 @@
+console.log("Hello, World!");
+console.warn("This is a warning message.");
+console.error("This is an error message.");
+
+const p = [{ name: "Alice", age: 30 }, { name: "Bob", age: 25 }];
+console.table(p);
